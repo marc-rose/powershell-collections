f4a66b067a1753217e92595d1be239a7865509fc
marc-rose <cram.esor@gmail.com>
2020-05-03 19:54:09 -0700

    Update ExcelToCsv.ps1
    changed script to one from https://zeleskitech.com/2014/10/26/convert-xlsx-csv-powershell/
    added notes to make sense of it

--- scripts/ExcelToCsv.ps1
+++ scripts/ExcelToCsv.ps1
@@ -1,14 +1,25 @@
-Function ExcelToCsv ($File) {
-    $myDir = "D:\Excel"
-    $excelFile = "$myDir\" + $File + ".xlsx"
+# This function requires Excel to be installed on the machine running the script
+# Place the .xlsx file in the same folder as script
+
+Function ExcelToCSV ($File) {
+ 
+    $excelFile = "$pwd\" + $File + ".xlsx"
+# set the file location (current folder) 
     $Excel = New-Object -ComObject Excel.Application
+# This code creates a COM object to use Excel features.
+    $Excel.Visible = $false
+    $Excel.DisplayAlerts = $false
+# Excel runs invisible and no displays no alerts while running macros
     $wb = $Excel.Workbooks.Open($excelFile)
-	
-    foreach ($ws in $wb.Worksheets) {
-        $ws.SaveAs("$myDir\" + $File + ".csv", 6)
+    foreach ($ws in $wb.Worksheets)
+    {
+        $ws.SaveAs("$pwd\" + $File + ".csv", 6) # use this line if one sheet needs to be converted
+#       $ws.SaveAs("$pwd\" + $File + "_" + $ws.name + ".csv", 6) # use this line if multiple sheets need to be converted
+# SaveAs uses 6 as a second parameter. You need to add it, so the file will be saved as a text form 
+# and the application will not try to save formatting.
     }
     $Excel.Quit()
 }
-
-$FileName = "list_of_names"
-ExcelToCsv -File $FileName
+$FileName = "Book1"
+ExcelToCSV -File "$FileName"
+# Should propably change to just pull all xlsx files in folder and convert them
